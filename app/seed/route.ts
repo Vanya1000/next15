50fb56144423e428b5ab69ad131a118ee00756f6
Ivan Zakharanka <ivanzakharanka@gmail.com>
2024-10-09 21:51:54 +0200

    chore: fix linter errors

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -120,28 +120,3 @@ export async function GET() {
   //   return Response.json({ error }, { status: 500 });
   // }
 }
-
-async function createUser() {
-  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-  await client.sql`
-    CREATE TABLE IF NOT EXISTS users (
-      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-      name VARCHAR(255) NOT NULL,
-      email TEXT NOT NULL UNIQUE,
-      password TEXT NOT NULL
-    );
-  `;
-
-  const insertedUsers = await Promise.all(
-    users.map(async (user) => {
-      const hashedPassword = await bcrypt.hash(user.password, 10);
-      return client.sql`
-        INSERT INTO users (id, name, email, password)
-        VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
-        ON CONFLICT (id) DO NOTHING;
-      `;
-    }),
-  );
-
-  return insertedUsers;
-}
